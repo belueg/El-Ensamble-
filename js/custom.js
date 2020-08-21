00f7b5347914296ad466f2bd930851a19c5ef056
Pau Joan <pregonipaw@hotmail.com>
2020-08-21 19:45:59 +0200

    Minor: changes in custom.js, bootstrap.css and ignored .vscode files.

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,10 @@
 // to get current year
 function getYear() {
-    var currentDate = new Date();
-    var currentYear = currentDate.getFullYear();
-    document.querySelector("#displayYear").innerHTML = currentYear;
+  var currentYear = new Date().getFullYear()
+  document.querySelector('#displayYear').innerHTML = currentYear
 }
-
-getYear();
-
 // nav menu
 function openNav() {
-    document.getElementById("myNav").classList.toggle("menu_width");
-    document
-        .querySelector(".custom_menu-btn")
-        .classList.toggle("menu_btn-style");
+  document.getElementById('myNav').classList.toggle('menu_width')
+  document.querySelector('.custom_menu-btn').classList.toggle('menu_btn-style')
 }
